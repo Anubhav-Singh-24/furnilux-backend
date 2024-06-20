07a7dfef3679cb3a5f05f3a4c97d5f2b753d61af
Anubhav <anubhavsingh.2406@gmail.com>
2024-06-20 21:11:17 +0530

    Update CORS policy and secure cookie settings for deployment

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({origin:'http://localhost:5173',credentials:true}));
+app.use(cors({ origin: "https://furni-lux.netlify.app", credentials: true }));
 app.use(cookieParser())
 
 const PORT = process.env.PORT || 3000;
