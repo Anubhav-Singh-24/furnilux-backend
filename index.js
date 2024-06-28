cf54ddb0b9eefbb0c425dbe88f803f95bbf39466
Anubhav <anubhavsingh.2406@gmail.com>
2024-06-28 12:51:56 +0530

    Minor changes to cors policy

--- index.js
+++ index.js
@@ -3,14 +3,13 @@ import cors from 'cors';
 import ConnectToDb from './db.js';
 import router from './routes/router.js';
 import cookieParser from 'cookie-parser';
-import helmet from "helmet";
-
+import helmet from 'helmet';
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://furni-lux.netlify.app", credentials: true }));
 app.use(cookieParser())
 app.use(helmet());
 
